8cea28a48a170419b91f4dcbd85b46ca2361d2dd
Etienne Fachaux <etienne@skyloud.fr>
2020-10-30 12:17:17 +0100

    feat: code architecture updated

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,60 @@
-import { connectCache } from './services/cache';
-import { connectEndpoint } from './services/endpoint';
-import { createInstance } from './instance';
-import {
-  SetupSkyenvProps,
-  SkyenvInstance,
-  SkyenvDefaultStore,
-} from './lib/interfaces';
+import axios from 'axios';
 
-class SkyenvSingletonInstance implements SkyenvInstance {
-  private _instance: SkyenvInstance<SkyenvDefaultStore>;
-  public store: SkyenvDefaultStore;
+const SKYENV_ENDPOINT_URL = 'https://vault.skyloud.net';
 
-  public async setup(config: SetupSkyenvProps) {
-    const endpoint = await connectEndpoint<SkyenvDefaultStore>(config);
-    const cache = await connectCache<SkyenvDefaultStore>(config);
-    
-    this._instance = createInstance<SkyenvDefaultStore>({ endpoint, cache });
-    this.store = this._instance.store;
-
-    return this;
-  }
-
-  public get(key: string, defaultValue?: string) {
-    if ( ! this._instance ) throw 'Please use setup before using get.';
-    return this._instance.get(key, defaultValue);
-  }
-
-  public reload() {
-    if ( ! this._instance ) throw 'Please use setup before using get.';
-    return this._instance.reload();
-  }
+interface SkyenvStore {
+    [key: string]: string
 }
 
-export default new SkyenvSingletonInstance();
+interface SkyenvResolver {
+    handler: (config: SkyenvConfig) => Promise<SkyenvStore>
+}
+
+interface SkyenvConfig {
+    accessId: string
+    accessSecret: string
+    endpointUrl?: string
+}
+
+export const systemEnvResolver: SkyenvResolver = {
+    handler: async () => {
+        return process.env;
+    }
+}
+
+export const cloudEnvResolver: SkyenvResolver = {
+    handler: async (config: SkyenvConfig) => {
+        const instance = axios.create({
+            baseURL: config.endpointUrl || SKYENV_ENDPOINT_URL,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+      
+          const response = await instance.post('/api/copy', {
+            accessId: config.accessId,
+            accessSecret: config.accessSecret,
+          });
+      
+          return response.data;
+    }
+}
+
+export let env: SkyenvStore = {};
+
+async function skyenv(config: SkyenvConfig, resolvers = [
+    systemEnvResolver,
+    cloudEnvResolver,
+]): Promise<SkyenvStore> {
+    env = {};
+    for (const resolver of resolvers) {
+        const envState = await resolver.handler(config);
+        env = {
+            ...env,
+            ...envState,
+        };
+    }
+    return env;
+}
+
+export default skyenv;
