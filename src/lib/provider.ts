014c1d13e79ff310b5f4f99a90d832a16da98580
Etienne Fachaux <etienne@skyloud.fr>
2020-10-30 12:16:06 +0100

    chore: cleaning code

--- src/lib/provider.ts
+++
@@ -1,8 +0,0 @@
-export function get<T>(store: T) {
-  return (key: keyof T, defaultValue: T[keyof T] = undefined): T[keyof T] | undefined => {
-    if (!store || !key) {
-      return defaultValue;
-    }
-    return store[key];
-  }
-}
--- src/test2.ts
+++
@@ -1,6 +0,0 @@
-import skyenv from '.';
-
-export default function test(): void {
-  const myKey = skyenv.get('BENOIT');
-  console.log({ myKey });
-}
